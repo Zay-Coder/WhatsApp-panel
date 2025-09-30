b2301257072210cfeaf7044ca6e96adbfb1dd261
Zay-Coder <aigantengkali22@gmail.com>
2025-09-30 15:00:48 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+  "name": "whatsapp-panel",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.5",
+    "express": "^4.18.2",
+    "qrcode": "^1.5.3"
+  }
+      }
